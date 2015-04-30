e6aa340f225337cc530a34b1615f8637c463624f
ilpalermitano <ilpalermitano@libero.it>
2015-04-30 11:51:29 +0200

    ciao
    primo commento

---
+++ while.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 22/04/2015
+ * Time: 13:28
+ */
+
+$indice=1;
+
+    while($indice<=10){
+        $risultato=7*$indice;
+        echo"7 *".$indice."=".$risultato."<br>";
+        $indice++;
+
+    }
